9032053310a21aa63c7be88fda174e8e54824c06
kelbar13 <32333560+kelbar13@users.noreply.github.com>
2018-03-10 19:42:16 -0500

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,6 +1,5 @@
-# Enter import modules
+# Dependencies
 import csv
-from datetime import datetime
 
 # Path to collect data from folder
 budgetCSV = ("budget_data_1.csv")
@@ -8,16 +7,29 @@ budgetCSV = ("budget_data_1.csv")
 # Read in the CSV file
 with open(budgetCSV, newline="") as csvfile:
   csvreader= csv.reader(csvfile, delimiter=",")
-  for row in csvreader:
-    # Total number of months included in the dataset
-    
-    # Total amount of revenue gained over the entire period
-    # Average change in revenue between months over the entire period
-    # Greatest increase in revenue (date and amount) over the entire period
-    # Greatest decrease in revenue (date and amount) over the entire period
-    # Print Total Months:, Total Revenue:, Average Revenue Change:, Greatest Increase in Revenue:, Greatest Decrease in Revenue:
 
+  # define the function 
+  def getTotals(budgetData)
 
+      # Total number of months included in the dataset
+      totalMonths = len(set(budgetData[1])
+
+      # Average change in revenue between months over the entire period
+      avgRevenue = (int(budgetData[2])/totalMonths)
+
+      # Greatest increase in revenue (date and amount) over the entire period
+      increase = (max(budgetData[2]))
+      
+      # Greatest decrease in revenue (date and amount) over the entire period
+      decrease = (min(budgetData[2]))
+
+      # Print Total Months:, Total Revenue:, Average Revenue Change:, Greatest Increase in Revenue:, Greatest Decrease in Revenue
+      print("Financial Analysis")  
+      print("---------------------------")
+      print("Total Months: " + str(totalMonths))
+      print("Total Revenue: " + str(avgRevenue))
+      print("Greatest Increase in Revenue: " + budgetData[0] + "(" + str(increase) + ")")
+      print("Greatest Decrease in Revenue: " + budgetData[0] + "(" + str(decrease) + ")")
   
   # Split the data on commas
   csvreader = csv.reader(csvfile, delimiter=',')
